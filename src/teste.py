187c3e9763e4d5c117f2dc416c0e7b7d06d6f43a
ira27 <iransantosjr@gmail.com>
2020-04-07 01:20:30 -0300

    travis

--- src/teste.py
+++ src/teste.py
@@ -1,2 +1,21 @@
-#este he o progama de testes dos numeros primos
+import jogovelha
+import sys
+
+erroInicializar = False
+jogo = jogovelha.inicializar()
+
+if len(jogo) != 3:
+    erroInicializar = True
+else:
+    for linha in jogo:
+        if len(linha) != 3:
+            erroInicializar = True
+        else:
+            for elemento in linha:
+                if elemento != '.':
+                    erroInicializar = True
+                    if erroInicializar:
+                        sys.exit(1)
+                    else:
+                        sys.exit(0)
 
